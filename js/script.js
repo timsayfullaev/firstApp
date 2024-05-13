af6eb54979e465500eadcf28633e1119d7eb6f61
Timur <tim.sayfullaev@gmail.com>
2024-05-13 23:20:31 +0500

    add gitignore

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', ''),
+    personalMovieDB = {
+        'count': numberOfFilms,
+        'movies': {},
+        'actors': {},
+        'genres': [],
+        'privat': false
+    };
+
