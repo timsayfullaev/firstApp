6430c9a0a80aad1d05b5c529e331285867b3e353
Timur <tim.sayfullaev@gmail.com>
2024-05-30 23:33:47 +0500

    add two cycles

--- js/script.js
+++ js/script.js
@@ -21,14 +21,47 @@ for (let i = 0; i < 2; i++) {
         console.log('error');
         i--;
     }
-
 }
+
+// let i = 0;
+
+// do {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+
+//     i++;
+// } while (i < 2);
+
+// let i = 0;
+
+// while (i < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+
+//     i++;
+// }
 
 if (personalMovieDB.count < 10) {
     console.log('Просмотрено довольно мало фильмов');
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
     console.log('Вы классический зритель');
-} else if (personalMovieDB.count >=30) {
+} else if (personalMovieDB.count >= 30) {
     console.log('Вы киноман');
 } else {
     console.log('Произошла ошибка');
